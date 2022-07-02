e597cbbf28462bafa9c2a72ae74e7f21645a08c6
Sanya <sssaaasha@mail.ru>
2022-07-02 14:40:42 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" ,"" );
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("последний из просмотренных фильмов" ,""),
+        b = prompt("На сколько оцените его?" ,"");
+        
+    if (a != null && b != null && a != '' && b != '' && a.length > 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+} else {
+    console.log('Error');
+    // i--;
+}
+
+}
+console.log(personalMovieDB);
